b4132e1254975ad4f9c0d4896ede386a89069294
akshatsinghania <you@example.com>
2021-09-28 12:54:59 +0530

    improved table and added toggle

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,39 +1,66 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Alert, Col, Container, Row } from "react-bootstrap";
 import "./Home.css";
+import { GrAdd } from "react-icons/gr";
 
 const Home = () => {
   const [maze, setMaze] = useState([
     [0, 0, 0, 0],
+    [1, 0, 1, 0],
     [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [0, 1, 9, 0],
   ]);
-  const [colTraced, setColTraced] = useState([]);
-  useEffect(() => {
-    setTimeout(() => {
-      setColTraced([[1, 1]]);
-    }, 3000);
-  }, []);
+  const [colTraced, setColTraced] = useState({
+    "1r1c": true,
+  });
+  const [error, setError] = useState("");
+
+  const addCol = (rowI) => {
+    var newMaze = [...maze];
+    if (newMaze[rowI].length < 28) {
+      newMaze[rowI].push(0);
+      setMaze(newMaze);
+    }
+  };
+  const toggleCol = (rowI, colI) => {
+    var newMaze = [...maze];
+    var col = maze[rowI][colI];
+    if (col === 0) col = 1;
+    else if (col === 1) col = 9;
+    else if (col == 9) col = 0;
+    newMaze[rowI][colI] = col;
+    setMaze(newMaze);
+  };
   return (
-    <Container className="maze">
-      {maze.map((row, rowI) => (
-        <Row>
-          {row.map((col, colI) => {
-            let current = false;
-            colTraced.findIndex((col) => {
-              current = col[0] == rowI && col[1] == colI;
-            });
-            console.log(current);
-            return (
-              <Col className={`maze__col ${current && "maze__col-current"}`}>
-                {col}
-              </Col>
-            );
-          })}
-        </Row>
-      ))}
-    </Container>
+    <>
+      {error && (
+        <Alert variant="danger" onClose={() => setError("")} dismissible>
+          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <p>{error}</p>
+        </Alert>
+      )}
+      <div className="maze">
+        {maze.map((row, rowI) => (
+          <div className="maze__row" key={rowI}>
+            {row.map((col, colI) => {
+              return (
+                <div
+                  onClick={() => toggleCol(rowI, colI)}
+                  key={colI}
+                  className={`maze__col 
+                ${col === 0 && "maze__col-path"} 
+                ${col === 1 && "maze__col-obstacle"} 
+                ${col === 9 && "maze__col-end"} 
+                ${colTraced[`${rowI}r${colI}c`] && "maze__col-current"}`}
+                />
+              );
+            })}
+
+            <GrAdd className="maze__add" onClick={() => addCol(rowI)} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
